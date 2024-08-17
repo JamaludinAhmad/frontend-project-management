f0ba8ba6819d14b3ddc3b3439ccaeb5c2fdd3123
anonruff <jamaludinahmad013@gmail.com>
2024-08-17 19:38:13 +0700

    first init

---
+++ application/helpers/Api_helper.php
@@ -0,0 +1,15 @@
+<?php
+
+use GuzzleHttp\Client;
+
+function call_api($url, $method, $data = false){
+    $client = new Client();
+    $response = $client->request($method, $url, [
+        'headers' => [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ],
+        'json' => $data
+    ]);
+    return json_decode($response->getBody()->getContents());
+}
