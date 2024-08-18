c0772698d8e3cd5e6d1f1f285f7742e64ed475f6
anonruff <jamaludinahmad013@gmail.com>
2024-08-18 08:45:12 +0700

    chage: url action

--- application/views/proyek/proyek_list.php
+++ application/views/proyek/proyek_list.php
@@ -20,7 +20,7 @@
                 <p class="mt-2 text-sm text-gray-700">A list of all the project.</p>
             </div>
             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                <a href="<?php echo(base_url ('proyek/create'))?>">
+                <a href="<?php echo(base_url ('index.php/proyek/create'))?>">
                     <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add project</button>
                 </a>
             </div>
